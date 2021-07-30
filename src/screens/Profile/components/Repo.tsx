ab7da5006f1aa0aa0bee8364b5867d4492077ab7
harshgoel05 <harshgoel05@gmail.com>
2021-07-30 20:49:27 +0530

    💄 Fix reponsive

--- src/screens/Profile/components/Repo.tsx
+++ src/screens/Profile/components/Repo.tsx
@@ -5,7 +5,7 @@ import Fork from "../../../assets/images/fork.svg";
 export default function Repo({ repo }: any) {
   return (
     <a href={repo.html_url} target="_blank" rel="noreferrer">
-      <div className="bg-white border rounded border-lightgray flex flex-col w-64 md:w-11/12 h-48 lg:w-96 p-6 m-2 justify-between">
+      <div className="bg-white border rounded border-lightgray flex flex-col w-64  h-48 lg:w-96 p-6 m-2 justify-between">
         <h1 className="font-medium gradient-text text-xl">{repo.name}</h1>
         <p className="text-darkgray break-words">
           {repo.description ||
